c18d7eb964d7c8a978e2df98a84155c11533c812
lpruim3 <lpruim3@gmail.com>
2024-05-23 21:53:33 -0300

    .

--- Estat.R
+++ Estat.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(dplyr)
 library(stringr)
 library(forcats)
+library(tibble)
 
 #ANALISE 1
 
@@ -59,51 +60,127 @@ graf <- banco %>%
     tipos %>% str_detect("CrossOver") ~ "CrossOver",
     
   )) %>%
-  group_by(decada,tipos) %>%
-  summarise(freq = n()) %>%
-  mutate(
-    freq_relativa = round(freq / sum(freq) * 100,1)
-  )
+  group_by(decada,tipos)
+  
 
+graf <- banco %>%
+  select(decada, season)
 porcentagens <- str_c(graf$freq_relativa, "%") %>% str_replace("\\.", ",")
 
 legendas <- str_squish(str_c(graf$freq, " (", porcentagens, ")"))
+
+
+print_quadro_resumo <- function(data, var_name, title="Medidas resumo da(o) [nome da variável]", label="quad:quadro_resumo1")
+{
+  var_name <- substitute(var_name)
+  data <- data %>%
+    summarize(`Média` = round(mean(!!sym(var_name)),2),
+              `Desvio Padrão` = round(sd(!!sym(var_name)),2),
+              `Variância` = round(var(!!sym(var_name)),2),
+              `Mínimo` = round(min(!!sym(var_name)),2),
+              `1º Quartil` = round(quantile(!!sym(var_name), probs = .25),2),
+              `Mediana` = round(quantile(!!sym(var_name), probs = .5),2),
+              `3º Quartil` = round(quantile(!!sym(var_name), probs = .75),2),
+              `Máximo` = round(max(!!sym(var_name)),2)) %>%
+    t() %>% 
+    as.data.frame() %>%
+    rownames_to_column()
+  
+  latex <- str_c("\\begin{quadro}[H]
+\t\\caption{", title, "}
+\t\\centering
+\t\\begin{adjustbox}{max width=\\textwidth}
+\t\\begin{tabular}{", sep="")
+  
+  col_count <- ncol(data)
+  row_count <- nrow(data)
+  latex <- str_c(latex, "| l |\n", sep=" ")
+  for (i in seq(2, col_count))
+  {
+    numCount <- data[i, -c(1)] %>%
+      as.numeric() %>%
+      {floor(log10(.)) + 1} %>%
+      max()
+    latex <- str_c(latex, "\t\t\tS[table-format = ", numCount ,".2]\n", sep="")
+  }
+  
+  
+  latex <- str_c(latex, "\t\t\t|}\n\t\\toprule\n\t\t", sep="")
+  if (col_count > 2)
+  {
+    for (i in seq(1,col_count))
+    {
+      if (i == 1)
+        latex <- str_c(latex, "\\textbf{Estatística}", sep="")
+      else
+        latex <- str_c(latex, " \\textbf{", data[1, i], "}", sep="")
+      
+      if (i < col_count)
+        latex <- str_c(latex, "&", sep=" ")
+      else
+        latex <- str_c(latex, "\\\\\n", sep=" ")
+    }
+  }
+  else
+  {
+    latex <- str_c(latex, "\\textbf{Estatística} & \\textbf{Valor} \\\\\n", sep="")  
+  }
+  
+  latex <- str_c(latex, "\t\t\\midrule\n", sep="")
+  
+  if (col_count > 2)
+    starting_number <- 2
+  else
+    starting_number <- 1
+  
+  for (i in seq(starting_number, row_count))
+  {
+    latex <- str_c(latex, "\t\t", str_flatten(t(data[i,]), collapse = " & "), " \\\\\n")
+  }
+  latex <- str_c(latex, "\t\\bottomrule
+\t\\end{tabular}
+\t\\label{", label, "}
+\t\\end{adjustbox}
+\\end{quadro}", sep="")
+  
+  writeLines(latex)
+}
+
+
+
 ggplot(graf) +
-  aes(
-    x = fct_reorder(decada, freq, .desc = TRUE), y = freq,
-    fill = tipos, label = legendas
-  ) +
-  geom_col(position = position_dodge2(preserve = "single", padding = 0)) +
-  geom_text(
-    position = position_dodge(width = 0.9),
-    vjust = -0.5, hjust = 0.5,
-    size = 2
-  ) +
+  aes(x = decada, y = freq, group = tipos, colour = tipos) +
+  geom_line(size = 1) +
+  geom_point(size = 2) +
   labs(x = "Decadas", y = "Frequência") +
   theme_estat()
-
-ggsave("resultado1.pdf", width = 158, height = 93, units = "mm")
-
+ggsave("series_grupo.pdf", width = 158, height = 93, units = "mm")
 #ANALISE 2 
+mean_ratings %>% 
+  group_by(season) %>% # caso mais de uma categoria
+  print_quadro_resumo(var_name = imdb)
 
-mean_ratings <- aggregate(imdb ~ season, data = banco, FUN = mean)
+mean_ratings <- banco %>%
+  select(season, imdb)
 mean_ratings <- mean_ratings %>%
   mutate(season = case_when(
-    season %>% str_detect("Special") ~ "Especial",
-    season %>% str_detect("Movie") ~ "Filme",
-    season %>% str_detect("Crossover") ~ "CrossOver",
     season %>% str_detect("1") ~ "1",
     season %>% str_detect("2") ~ "2",
     season %>% str_detect("3") ~ "3",
     season %>% str_detect("4") ~ "4"))
-
-
+mean_ratings <- na.omit(mean_ratings)
 ggplot(mean_ratings) +
-  aes(x=season, y=imdb, group=1) +
-  geom_line(size=1,colour="#A11D21") + geom_point(colour="#A11D21",size=2) +
-  labs(x="Temporadas", y="IMDb Médio") +
+  aes(x = reorder(season, imdb, FUN = median), y = imdb) +
+  geom_boxplot(fill = c("#A11D21"), width = 0.5) +
+  stat_summary(
+    fun = "mean", geom = "point", shape = 23, size = 3, fill = "white"
+  ) +
+  labs(x = "Temporadas", y = "Nota Imdb") +
   theme_estat()
-ggsave("resultado2.pdf", width = 158, height = 93, units = "mm")
+ggsave("box_bi.pdf", width = 158, height = 93, units = "mm")
+mean_ratings %>% 
+  group_by(season) %>% # caso mais de uma categoria
+  print_quadro_resumo(var_name = imdb)
 
 #ANALISE 3 
 
@@ -151,16 +228,17 @@ ggsave("colunas-bi-freq.pdf", width = 158, height = 93, units = "mm")
 
 ggplot(banco) +
   aes(x = imdb, y = engagement) +
-  geom_point(colour = "#A11D21", size = 3) +
+  geom_point(colour = "#A11D21", size = 3,alpha = 0.5) +
   labs(
     x = "Notas IMDB",
     y = "Engajamento"
   ) +
   theme_estat()
-ggsave("disp_uni.pdf", width = 158, height = 93, units = "mm")
+ggsave("disp_1uni.pdf", width = 158, height = 93, units = "mm")
 
 
-
+banco %>% 
+  print_quadro_resumo(var_name = imdb)
 
 
 
